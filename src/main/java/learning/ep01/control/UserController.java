7673b97fd8a298f6e3e4e8652d25190d31060611
theguefche <university.info.alaa@gmail.com>
2023-02-09 14:50:24 +0100

    load

--- src/main/java/learning/ep01/control/UserController.java
+++ src/main/java/learning/ep01/control/UserController.java
@@ -2,14 +2,19 @@ package learning.ep01.control;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import learning.ep01.model.User;
 import learning.ep01.repository.UserRepository;
@@ -25,9 +30,19 @@ public class UserController {
     }
 
     @PostMapping("/AddUser")
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
+    public User createUser(@RequestBody User user,HttpServletResponse response) throws InterruptedException {
+    
+                try {
+                    userRepository.save(user);
+                } catch (Exception e) {
+                    throw new ResponseStatusException(
+                            HttpStatus.INTERNAL_SERVER_ERROR,
+                            "Failed to save request", e);
+                }
+                return user;
+
     }
+
 
     @DeleteMapping("/users/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable Long id) {
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
     $.ajax({
         url: "/users",
@@ -17,12 +18,14 @@ $(document).ready(function () {
 
     });
 });
+
 $("#add-user-form").submit(function (event) {
     event.preventDefault();
     $.ajax({
         url: "/AddUser",
         type: "POST",
         contentType: "application/json",
+        timeout: 1000,
         data: JSON.stringify({
             name: $("#name").val()
 
@@ -35,48 +38,40 @@ $("#add-user-form").submit(function (event) {
                 "<td><button class='delete-button' data-user-id='" + user.id + "' onclick='deleteUser()' >Delete</button></td>" +
                 "</tr>"
             );
+            Swal.fire({
+                icon: 'success',
+                title: 'noice',
+                text: 'Cool!',
+              })
+        },
+
+        
+        error : function (xhr, status, error) {
+            Swal.fire({
+                icon: 'info',
+                title: 'noice',
+                text: 'Cool!',
+              })
         }
     });
 });
-// $(".delete-button").click(function (e) {
-//     e.preventDefault();
-//     let userId = $(this).data("id");
-//     $.ajax({
-//         url: "/users/" + userId,
-//         type: "DELETE",
-//         success: function (result) {
-//             console.log("User deleted");
-//             // Remove the user from the list
-//             $("button[data-user-id='" + userId + "']").closest("tr").remove();
-//         }
-//     });
 
 
-// });
 
-$('.delete-button2').click(function (e) {
-    e.preventDefault();
-    let userId = $(this).data("user-id");
-    alert(userId);
-    alert(this);
-
-});
-function deleteUser() {
-    alert(event);
-    // e.preventDefault();
-    let userId = $(event).data("user-id");
-    alert(userId);
-    // alert($("h1").data("id"))
-    // let userId = $(this).data("user-id");
-    // alert(userId);
-    // $.ajax({
-    //     url: "/users/" + userId,
-    //     type: "DELETE",
-    //     success: function (result) {
-    //         console.log("User deleted");
-    //         // Remove the user from the list
-    //         $("button[data-user-id='" + userId + "']").closest("tr").remove();
-    //     }
-    // });
-
+function deleteUser() {   
+    var userid = event.target.getAttribute("data-user-id");
+    $.ajax({
+        url: "/users/"+userid,
+        type: "DELETE",
+        contentType: "application/json",
+       
+        success: function (user) {
+            
+            Swal.fire({
+                icon: 'success',
+                title: 'noice',
+                text: 'Cool!',
+              })
+        },
+    });
 }
